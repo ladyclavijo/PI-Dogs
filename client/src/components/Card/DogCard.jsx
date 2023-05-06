0c365703a5e49786235dd798691e59e787946f65
Lady Clavijo <layclavijo10@gmail.com>
2023-05-06 06:41:45 -0300

    act

--- client/src/components/Card/DogCard.jsx
+++ client/src/components/Card/DogCard.jsx
@@ -1,17 +1,17 @@
+import "./card.css";
 import React from "react";
 // import { Link } from "react-router-dom";
 
-const DogCard = ({name, image, temperaments, weight}) => { // me lo traigo por props
+const DogCard = ({name, image, life_span, temperaments, height, weight}) => { // me lo traigo por props
 
     let temps;
     if (typeof temperaments === "string") {
         temps = temperaments.split(", ");
-        console.log(temps + "entré al if")
+        // console.log(temps + "entré al if")
     } else { 
         temps = temperaments
-        console.log(temperaments + "no entré al if")
+        // console.log(temperaments + "no entré al if")
     }
-
 
     return (
       <div className="container-card">
@@ -19,20 +19,23 @@ const DogCard = ({name, image, temperaments, weight}) => { // me lo traigo por p
         <img className="container-image__img" src={image} alt= "img not found"/>
         </div>
 
-        <div className="container-info">
-            {/* <p className="card-text">ID: {id}</p> */}
-            <p className="card-text">Name: {name}</p>
+        <div className="info-card">
+            <p className="card-title">{name}</p>
+        
+        <div className="middle-cards">
+
+            <p className="card-text">Life_span: {life_span}</p>
+            <p className="card-text">Height: {height}</p>
+            <p className="card-text">Weight: {weight}</p>
         </div>
 
         <div className="temperaments-div">
-            <p>Temperaments: </p>
+            <p className="temps-title">Temperaments: </p>
             {temps?.map(e => {
-                return <p key={e.name ? e.name : e}>{e.name ? e.name : e}</p>
+                return <p className="temps" key={e.name ? e.name : e}>{e.name ? e.name : e}</p>
             })}
         </div>
 
-        <div>
-            <p className="card-text">Weight: {weight}</p>
         </div>
       </div>
     );
